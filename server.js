02a59b4eb9d58e5f4c896c43838cc6c63742c737
mob35 <toonelicious@gmail.com>
2019-01-10 12:00:59 +0700

    server Template

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 const app = require('./src/config/app');
 const server = require('http').createServer(app);
-const socket = require('./src/modules/chat/controllers/socket-chat-controller')(server);
 server.listen(process.env.PORT || 3000, function () {
     console.log('Start server');
     console.log('Server is running');
