3591d89a52d61ecba0495ab4f58b006f4ac7d239
mob35 <toonelicious@gmail.com>
2019-01-10 12:32:40 +0700

    test

--- src/modules/notify/routes/route.js
+++ src/modules/notify/routes/route.js
@@ -6,14 +6,44 @@ var _model = require('../models/model').model,
 module.exports = function (app) {
     var url = '/api/' + _model;
     var urlWithParam = '/api/' + _model + '/:' + _model + 'id';
-    app.route(url).all(core.jwtCheck, policy.isAllowed)
-        .get(controller.getList)
-        .post(controller.create);
+    // app.route(url).all(core.jwtCheck, policy.isAllowed)
+        // .get(controller.getList)
+        // .post(controller.create);
 
-    app.route(urlWithParam).all(core.jwtCheck, policy.isAllowed)
-        .get(controller.read)
-        .put(controller.update)
-        .delete(controller.delete);
+    // app.route(urlWithParam).all(core.jwtCheck, policy.isAllowed)
+    //     .get(controller.read)
+    //     .put(controller.update)
+    //     .delete(controller.delete);
+    app.route('/api/notify').all(core.jwtCheck, policy.isAllowed)
+    .post(controller.create);
+
+    app.route('/api/notify2').post('/', function(req, res, next) {
+        var token = 'cuHsIVD1yGgwnzEuSQ13Br9Y4T4ZE2lHVfN4T9MzNaB';
+        var message = 'req.body.message';
+       
+        request({
+          method: 'POST',
+          uri: 'https://notify-api.line.me/api/notify',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          auth: {
+            'bearer': token
+          },
+          form: {
+            message: message
+          }
+        }, (err, httpResponse, body) => {
+          if(err){
+            console.log(err);
+          } else {
+            res.json({
+              httpResponse: httpResponse,
+              body: body
+            });
+          }
+        });
+      });
 
     app.param(_model + 'id', controller.getByID);
 }
